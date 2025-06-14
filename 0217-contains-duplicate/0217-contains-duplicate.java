f578a48499c368c537462c99980a702704291417
Shashank Hegde <86239282+ShashankHegde8@users.noreply.github.com>
2025-06-14 23:53:47 +0530

    Time: 13 ms (86.73%), Space: 58 MB (84.53%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,25 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-      HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if (set.contains(num)) {
+        HashSet<Integer> h1=new HashSet<>();
+        // for(int i:nums){
+        //     if(h1.contains(nums[i])){
+        //         return true;
+        //     }
+        //     else{
+        //         h1.add(nums[i]);
+        //     }
+        // }
+        // return false;
+
+        for(int i=0;i<nums.length;i++){
+            if(h1.contains(nums[i])){
                 return true;
             }
-            set.add(num);
+            else{
+                h1.add(nums[i]);
+            }
+
         }
         return false;
-}
+    }
 }
